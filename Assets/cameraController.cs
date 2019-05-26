08374d112d561a7cfdd1c7ea7fa3a755b2186dc9
Sorrowsouls <alexdummett@hotmail.com>
2019-05-26 10:58:18 +0100

    Initial commit

---
+++ Assets/cameraController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    public float cameraMoveSpeed = 100.0f;
+    public GameObject CameraFollowObj;
+    Vector3 followPosition;
+    public float clamAngle = 80.0f;
+    public float inputSensitivity = 150.0f;
+    public GameObject playerObject;
+    public float camDistanceXToPlayer;
+    public float camDistanceZToPlayer;
+    public float camDistanceYToPlayer;
+    public float mouseX;
+    public float mouseY;
+    public float finalInputX;
+    public float finalInputZ;
+    public float smoothX;
+    public float smoothY;
+    private float rotY = 0.0f;
+    private float rotX = 0.0f;
+
+    // in future will be better to get attempt to get via tag and handle if none found
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("RightStickHorizontal");
+        float inputZ = Input.GetAxis("RightStickVertical");
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+        finalInputX = inputX + mouseX;
+        finalInputZ = inputZ + mouseY;
+
+        rotY += finalInputX * inputSensitivity * Time.deltaTime;
+        rotX += finalInputZ * inputSensitivity * Time.deltaTime;
+        Debug.Log(inputX);
+        rotX = Mathf.Clamp(rotX, -clamAngle, clamAngle);
+
+        Quaternion localRotaion = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotaion;
+
+    }
+
+    void LateUpdate()
+    {
+        CameraUpdater();
+    }
+
+    void CameraUpdater()
+    {
+        Transform target = CameraFollowObj.transform;
+
+        float step = cameraMoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
---
+++ Assets/playerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+    private Vector3 moveDirection = Vector3.zero;
+    public float speed = 5.0f;
+    private Rigidbody playerRigidbody;
+    private Animator playerAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    // TODO: Add gravity, verticle solutions to levels would be fun
+    void move(float i, float j)
+    {
+        bool isRunning = i != 0 || j != 0;
+        playerAnimator.SetTrigger("Walk");
+
+        moveDirection.Set(i, 0.0f, j);
+
+        moveDirection = moveDirection.normalized * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(moveDirection + transform.position);
+
+        if (i != 0 || j != 0)
+        {
+            transform.forward = Vector3.Normalize(moveDirection);
+        }
+    }
+}
